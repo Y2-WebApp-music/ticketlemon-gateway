e43c98bbd371b42f1b658063100dc6272ec4241e
Ariez <kritamet.cho@gmail.com>
2026-05-08 19:56:17 +0700

    Add utils/formData.ts

--- src/utils/formData.ts
+++ src/utils/formData.ts
@@ -1,9 +1,20 @@
-export const createFormData = (data: Record<string, any>) => {
+export const createFormData = (data: Record<string, any>): FormData => {
   const formData = new FormData();
 
   Object.entries(data).forEach(([key, value]) => {
     if (value === undefined || value === null) return;
-    formData.append(key, value);
+
+    if (value instanceof File || value instanceof Blob) {
+      formData.append(key, value);
+      return;
+    }
+
+    if (typeof value === "object") {
+      formData.append(key, JSON.stringify(value));
+      return;
+    }
+
+    formData.append(key, String(value));
   });
 
   return formData;
